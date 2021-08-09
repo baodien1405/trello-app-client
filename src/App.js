1ceb90e8238f82883fe80cd14bc8eb269411f480
Dien Cap <baodien1405@gmail.com>
2021-08-09 15:08:58 +0700

    Clean architecture

--- src/App.js
+++ src/App.js
@@ -1,115 +1,15 @@
+import React from "react";
 import "./App.scss";
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="trello-app">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.unsplash.com/photo-1628414832152-aafba4dec0d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                alt="img-title"
-              />
-              Title: BAODIEN
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
